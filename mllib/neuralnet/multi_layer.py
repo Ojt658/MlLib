45751758e61096e652d3b460af091dd6412efb4b
olt13 <olt13@aber.ac.uk>
2020-10-12 18:41:16 +0100

    Added the majority of the MLP algorithm, plus a test for it

--- mllib/neuralnet/multi_layer.py
+++ mllib/neuralnet/multi_layer.py
@@ -1,3 +1,81 @@
-class MLP:
-    def __init__(self):
+import numpy as np
+
+
+class MultiLayerPerceptron:
+    def __init__(self, n_hidden=2, out_type='logistic', epochs=1000, beta=1, momentum=0.9, eta=0.2,
+                 early_stopping=False):
+        self.out_weights = 0
+        self.hidden_weights = 0
+        self.hidden = 0
+        self.outputs = 0
+
+        self.n_hidden = n_hidden
+        self.out_type = out_type
+
+        self.epochs = epochs
+        self.beta = beta
+        self.momentum = momentum
+        self.eta = eta
+        self.early_stopping = early_stopping
+
+        if self.early_stopping:
+            self.epochs = 100
+
+    def fit(self, inputs, targets):
+        num_in = np.shape(inputs)[1]
+        num_out = np.shape(targets)[1]
+        self.hidden_weights = (np.random.rand(num_in + 1, self.n_hidden) - 0.5) * 2 / np.sqrt(num_in)
+        self.out_weights = (np.random.rand(self.n_hidden + 1, num_out) - 0.5) * 2 / np.sqrt(self.n_hidden)
+
+        self.train(inputs, targets)
+
+    def train_early_stopping(self, inputs, targets, valid, valid_targets):
         pass
+
+    def train(self, inputs, targets):
+        num_data = np.shape(inputs)[0]
+        inputs = np.concatenate((inputs, -np.ones((num_data, 1))), axis=1)
+        out_delta = 0
+
+        update_hidden = np.zeros(np.shape(self.hidden_weights))
+        update_out = np.zeros(np.shape(self.out_weights))
+
+        for e in range(self.epochs):
+            self.outputs = self.predict(inputs)
+
+            error = 0.5 * np.sum((self.outputs - targets) ** 2)
+            if np.mod(e, 50) == 0:
+                print("Epoch: ", e, "Error: ", error)
+
+            if self.out_type == 'linear':
+                out_delta = (self.outputs - targets) / num_data
+            elif self.out_type == 'logistic':
+                out_delta = self.beta * (self.outputs - targets) * self.outputs * (1.0 - self.outputs)
+            elif self.out_type == 'softmax':
+                out_delta = (self.outputs - targets) * (self.outputs * (-self.outputs) + self.outputs) / num_data
+            else:
+                print('error')
+
+            hidden_delta = self.hidden * self.beta * (1.0 - self.hidden) * np.dot(out_delta, np.transpose(self.out_weights))
+
+            self.hidden_weights -= self.eta * (
+                np.dot(np.transpose(inputs), hidden_delta[:, :-1])) + self.momentum * update_hidden
+            self.out_weights -= self.eta * (
+                np.dot(np.transpose(self.hidden), out_delta)) + self.momentum * update_out
+
+    def predict(self, inputs):
+        self.hidden = np.dot(inputs, self.hidden_weights)
+        self.hidden = 1.0 / (1.0 + np.exp(-self.beta * self.hidden))
+        self.hidden = np.concatenate((self.hidden, -np.ones((np.shape(inputs)[0], 1))), axis=1)
+
+        outputs = np.dot(self.hidden, self.out_weights)
+
+        if self.out_type == 'linear':
+            return outputs
+        elif self.out_type == 'logistic':
+            return 1.0 / (1.0 + np.exp(-self.beta * outputs))
+        elif self.out_type == 'softmax':
+            normalisers = np.sum(np.exp(outputs), axis=1) * np.ones((1, np.shape(outputs)[0]))
+            return np.transpose(np.transpose(np.exp(outputs)) / normalisers)
+        else:
+            print("error")
--- tests/neural_net_tests.py
+++ tests/neural_net_tests.py
@@ -1,18 +1,46 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from mllib.neuralnet.perceptron import Perceptron
+from mllib.neuralnet.multi_layer import MultiLayerPerceptron
+
 
 def test_perceptron_on_OR():
-    inputs = [[0,0], [0,1], [1,0], [1,1]]
+    inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
     labels = [[0], [1], [1], [1]]
 
     pcn = Perceptron()
     pcn.fit(inputs, labels)
 
+
 def perceptron_test_with_2_neurons():
-    inputs = [[0,0], [0,1], [1,0], [1,1]]
+    inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
     labels = [[0], [1], [1], [1]]
     pcn = Perceptron(num_nodes=2)
     pcn.fit(inputs, labels)
 
-test_perceptron_on_OR()
-perceptron_test_with_2_neurons()
+
+def test_mlp():
+    mlp = MultiLayerPerceptron(out_type='linear', epochs=1001, n_hidden=2)
+
+    x = np.ones((1, 40)) * np.linspace(0, 1, 40)
+    t = np.sin(2*np.pi*x) + np.cos(4*np.pi*x) + np.random.randn(40)*0.2
+    x = x.T
+    t = t.T
+
+    train = x
+    # test = x[1::4, :]
+    # valid = x[3::4, :]
+    traintar = t
+    # testtar = t[1::4, :]
+    # validtar = t[3::4, :]
+
+    plt.scatter(train, traintar, s=10)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+    mlp.fit(train, traintar)
+
+# test_perceptron_on_OR()
+# perceptron_test_with_2_neurons()
+test_mlp()
