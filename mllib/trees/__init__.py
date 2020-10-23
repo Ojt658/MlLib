f6883e889ec0f6849a568ce881c71664fae760e5
ollie <ojt@btinternet.com>
2020-10-23 22:35:48 +0100

    Started work on id3

---
+++ mllib/trees/__init__.py
@@ -0,0 +1,5 @@
+from mllib.trees.decision_trees import DecisionTreeID3
+
+__all__ = [
+    'DecisionTreeID3'
+]
---
+++ mllib/trees/decision_trees.py
@@ -0,0 +1,12 @@
+class DecisionTreeID3:
+    def __init__(self):
+        pass
+
+    def _entropy(self, attribute):
+        pass
+
+    def fit(self, inputs, targets):
+        pass
+
+    def predict(self, inputs):
+        pass
